a8bca4cdc5f89145f5847dfc6f90502e82f0e49f
Kantemir Nagoev <kantyang@MacBook-Pro-Kantemir.local>
2023-08-21 18:30:38 +0300

    fix: 1tsk

--- 1tsk.js
+++ 1tsk.js
@@ -1,8 +1,10 @@
 const data = [5, 1, 7, 3, 10, 2, 9, 0];
+const data1 = [4, 3, 74, 23, 110, 42, 59, 0];
+const data2 = [125, 12, 127, 33, 101, 2, 39, 40];
 
-const sortB = (arr) => {
-  const len = arr.length;
-
+const sortB = (myArr) => {
+  const len = myArr.length;
+  const arr = [...myArr];
   for (let i = 0; i < len; i++) {
     for (let j = i + 1; j < len; j++) {
       if (arr[i] > arr[j]) {
@@ -10,8 +12,11 @@ const sortB = (arr) => {
       }
     }
   }
+
+  return arr;
 };
 
-sortB(data);
-
 console.log(data);
+console.log(sortB(data));
+console.log(sortB(data1));
+console.log(sortB(data2));
