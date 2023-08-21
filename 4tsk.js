f11d012616bc0e9b8de3ece1df0fed5fe2738177
Kantemir Nagoev <kantyang@MacBook-Pro-Kantemir.local>
2023-08-21 19:54:45 +0300

    feat: dfs

---
+++ 4tsk.js
@@ -0,0 +1,29 @@
+const data = {
+  val: 1,
+  left: {
+    val: 2,
+    left: { val: 4 },
+    right: { val: 5 },
+  },
+  right: {
+    val: 3,
+    left: { val: 6 },
+    right: { val: 7 },
+  },
+};
+
+const dfs = (obj) => {
+  const res = [];
+  res.push(obj.val);
+
+  if (obj.hasOwnProperty("left")) {
+    res.push(...dfs(obj.left));
+  }
+  if (obj.hasOwnProperty("right")) {
+    res.push(...dfs(obj.right));
+  }
+
+  return res;
+};
+
+console.log(dfs(data));
